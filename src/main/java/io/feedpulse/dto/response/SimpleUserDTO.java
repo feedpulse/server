db6c2b1b3f11fb23f83edc0592e8f06364cc76f4
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2024-02-29 21:21:54 +0100

    fix: add User ID in SimpleUserDTO
    The User ID is now encoded in Base64 in the SimpleUserDTO class. This change is applied within the 'of' method where a new SimpleUserDTO is created from a User object.

--- src/main/java/io/feedpulse/dto/response/SimpleUserDTO.java
+++ src/main/java/io/feedpulse/dto/response/SimpleUserDTO.java
@@ -3,9 +3,12 @@ package io.feedpulse.dto.response;
 import io.feedpulse.model.User;
 import io.feedpulse.model.Role;
 
+import java.util.Base64;
 import java.util.Set;
 
 public record SimpleUserDTO(
+
+        String id,
         String username,
         String email,
         Set<Role> role,
@@ -13,7 +16,9 @@ public record SimpleUserDTO(
         boolean locked) {
 
     public static SimpleUserDTO of(User user) {
+        String id = Base64.getEncoder().encodeToString(user.getId().toString().getBytes());
         return new SimpleUserDTO(
+                id,
                 user.getUsername(),
                 user.getEmail(),
                 user.getRoles(),
